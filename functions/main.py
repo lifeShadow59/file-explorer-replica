96f6b452aef24ff6df65d3a5b72c6da338b58f19
existence <abhishek9712a@gmail.com>
2021-08-29 19:38:28 +0530

    add safety index out of range

--- functions/main.py
+++ functions/main.py
@@ -16,24 +16,31 @@ lines =  myFile.readlines()
 #     print(x,end='')
 PATH:str = os.path.dirname(os.path.realpath(__file__))
 print(ord('│'))
-for i in range(1,60):
+for i in range(1,len(lines) - 2):
+    # dir
     print(i+1 , '    ' ,lines[i][indexValue:],end='')
-    path = os.path.join(PATH, lines[i][indexValue:][:-1])
-    os.mkdir(path)
-    asciiValue = getAsciiValue(lines[i+1][indexValue])
-    i-1
-    if asciiValue == 9500 or asciiValue == 9492 or asciiValue == 9474:
-        indexValue = indexValue + 4
+    # path = os.path.join(PATH, lines[i][indexValue:][:-1])
+    # os.mkdir(path)
 
 
-    xasciiValue = getAsciiValue(lines[i+1][indexValue-4])
-    i-1
-    if (xasciiValue == 9500 or xasciiValue == 9492 or xasciiValue == 9474):
-        pass
 
-    else:
-        indexValue = indexValue - 4
-        
+    
+
+    try:
+        asciiValue = getAsciiValue(lines[i+1][indexValue])
+        i-1
+        if asciiValue == 9500 or asciiValue == 9492 or asciiValue == 9474:
+            indexValue = indexValue + 4
+
+        xasciiValue = getAsciiValue(lines[i+1][indexValue-4])
+        i-1
+        if (xasciiValue == 9500 or xasciiValue == 9492 or xasciiValue == 9474):
+            pass
+
+        else:
+            indexValue = indexValue - 4
+    except IndexError:
+        print('index not in range')
 
 
 
