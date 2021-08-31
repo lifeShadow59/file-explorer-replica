c45553f5dfaa932540bf2ac9d46e4373d009a9d4
existence <abhishek9712a@gmail.com>
2021-09-01 00:09:55 +0530

    comment logic

--- functions/main.py
+++ functions/main.py
@@ -8,7 +8,7 @@ from get_ascii_value import getAsciiValue
 indexValue:int = 4
 
 
-myFile:io.TextIOWrapper = openFile(ConstantString.SOURCE_FILE_PATH,ConstantString.FILE_OPEN_IN_READ_AND_WRITE_MODE)
+myFile:io.TextIOWrapper = openFile(ConstantString.SOURCE_DIRS_PATH,ConstantString.FILE_OPEN_IN_READ_AND_WRITE_MODE)
 lines =  myFile.readlines()
 
 # for line in lines:
@@ -16,31 +16,33 @@ lines =  myFile.readlines()
 #     print(x,end='')
 PATH:str = os.path.dirname(os.path.realpath(__file__))
 print(ord('│'))
-for i in range(1,len(lines) - 2):
-    # dir
-    print(i+1 , '    ' ,lines[i][indexValue:],end='')
-    # path = os.path.join(PATH, lines[i][indexValue:][:-1])
-    # os.mkdir(path)
+
+# This is For get Files and folder list
+# for i in range(1,len(lines) - 2):
+#     # dir
+#     print(i+1 , '    ' ,lines[i][indexValue:],end='')
+#     # path = os.path.join(PATH, lines[i][indexValue:][:-1])
+#     # os.mkdir(path)
 
 
 
     
 
-    try:
-        asciiValue = getAsciiValue(lines[i+1][indexValue])
-        i-1
-        if asciiValue == 9500 or asciiValue == 9492 or asciiValue == 9474:
-            indexValue = indexValue + 4
+#     try:
+#         asciiValue = getAsciiValue(lines[i+1][indexValue])
+#         i-1
+#         if asciiValue == 9500 or asciiValue == 9492 or asciiValue == 9474:
+#             indexValue = indexValue + 4
 
-        xasciiValue = getAsciiValue(lines[i+1][indexValue-4])
-        i-1
-        if (xasciiValue == 9500 or xasciiValue == 9492 or xasciiValue == 9474):
-            pass
+#         xasciiValue = getAsciiValue(lines[i+1][indexValue-4])
+#         i-1
+#         if (xasciiValue == 9500 or xasciiValue == 9492 or xasciiValue == 9474):
+#             pass
 
-        else:
-            indexValue = indexValue - 4
-    except IndexError:
-        print('index not in range')
+#         else:
+#             indexValue = indexValue - 4
+#     except IndexError:
+#         print('index not in range')
 
 
 
